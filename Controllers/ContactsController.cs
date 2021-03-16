1a3cf5bafbf66a9965a5c672a8f9542d67b9c7de
Yurij <nabor_2008@i.ua>
2021-03-17 01:01:23 +0300

    add model and controller

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,85 @@
+﻿using InteractiveContaclListServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InteractiveContaclListServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+
+        ApplicationContext db;
+        public ContactsController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> Get()
+        {
+            return await db.Contacts.ToListAsync();
+        }
+
+        // GET api/Contacts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> Get(int id)
+        {
+            Contact Contact = await db.Contacts.FirstOrDefaultAsync(x => x.Id == id);
+            if (Contact == null)
+                return NotFound();
+            return new ObjectResult(Contact);
+        }
+
+        // POST api/Contacts
+        [HttpPost]
+        public async Task<ActionResult<Contact>> Post(Contact Contact)
+        {
+            if (Contact == null)
+            {
+                return BadRequest();
+            }
+
+            db.Contacts.Add(Contact);
+            await db.SaveChangesAsync();
+            return Ok(Contact);
+        }
+
+        // PUT api/Contacts/
+        [HttpPut]
+        public async Task<ActionResult<Contact>> Put(Contact Contact)
+        {
+            if (Contact == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Contacts.Any(x => x.Id == Contact.Id))
+            {
+                return NotFound();
+            }
+
+            db.Update(Contact);
+            await db.SaveChangesAsync();
+            return Ok(Contact);
+        }
+
+        // DELETE api/Contacts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Contact>> Delete(int id)
+        {
+            Contact Contact = db.Contacts.FirstOrDefault(x => x.Id == id);
+            if (Contact == null)
+            {
+                return NotFound();
+            }
+            db.Contacts.Remove(Contact);
+            await db.SaveChangesAsync();
+            return Ok(Contact);
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InteractiveContaclListServer.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Facebook { get; set; }
+        public string Instagram { get; set; }
+        public string Vk { get; set; }
+        public string LinkedIn { get; set; }
+        public string Twitter { get; set; }
+    }
+}
